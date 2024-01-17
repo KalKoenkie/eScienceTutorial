4f8b3a335bbbf2cba9c87b3e74950f4c1fa86cea
KalKoenkie <134587796+KalKoenkie@users.noreply.github.com>
2024-01-17 10:27:39 +0100

    Updates

--- R/script.R
+++ R/script.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(readxl)
 
 # Load in data from the xlsx files in the folder
-data <- list.files(path = "data", pattern = "*.xlsx", full.names = TRUE) %>% 
+data <- list.files(path = "data", pattern = "*.xlsx", full.names = TRUE) %>%
   map(read_xlsx)
 
 # For each file in the folder, pivot it wider and add it to a dataframe. Each file has a different values column name
@@ -15,14 +15,14 @@ data2 <- pivot_longer(data = data, cols = "Individual ID", names_to = "Variables
 
 # Solution
 
-get_observation <- function(filename){
-raw_file <- read_xlsx(path = filename)
+get_observation <- function(filename) {
+  raw_file <- read_xlsx(path = filename)
 
-transposed <- as.data.frame(t(raw_file))
+  transposed <- as.data.frame(t(raw_file))
 
-observation <- transposed[2,]
+  observation <- transposed[2, ]
 
-observation
+  observation
 }
 
 files <- list.files(path = "data/raw/datafiles/", pattern = "*.xlsx", full.names = TRUE)
@@ -34,6 +34,10 @@ for (file in files) {
   cleaned <- rbind(cleaned, row)
 }
 
+# Write the data to a csv file
 
 write.csv(cleaned, "data/clean/cleaned.csv")
 
+# Load the data in again
+
+cleaned <- read.csv("data/clean/cleaned.csv")
